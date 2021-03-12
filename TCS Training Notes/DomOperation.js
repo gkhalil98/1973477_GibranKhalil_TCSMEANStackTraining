178bbd3239f800abc8204bc7e7766cb211005c45
Gibran Khalil <gkhalil98@gmail.com>
2021-03-12 13:18:16 -0500

    added all my training files

---
+++ TCS Training Notes/DomOperation.js
@@ -0,0 +1,40 @@
+var empObj=[];
+
+function storeInSession() {
+  sessionStorage.setItem("empInfo",empObj);
+}
+
+function retrieveFromSession() {
+  var obj = sessionStorage.getItem("empInfo");
+}
+
+function onFormSubmit() {
+//  alert("Event generated...");
+  var data = readFormData();
+  insertNewRecord(data);
+  empObj.push(data);    //in empObj
+  resetData();
+}
+
+function readFormData() {
+  var obj = {}    //empty object
+  obj.name = document.getElementById("name").value;
+  obj.age = document.getElementById("age").value;
+  console.log(obj);
+  return obj;
+}
+
+function insertNewRecord(data) {
+  var table = document.getElementById("employeeList");
+  var body = table.getElementsByTagName("tbody")[0];
+  var newRow = body.insertRow(body.length);
+  var cell1 = newRow.insertCell(0);
+  cell1.innerHTML = data.name;
+  var cell2 = newRow.insertCell(1);
+  cell2.innerHTML = data.age;
+}
+
+function resetData() {
+  document.getElementById("name").value = "";
+  document.getElementById("age").value = "";
+}
